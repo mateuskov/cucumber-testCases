2fac8d0ef75331ae2442b602187bc2d0e82b441a
Mateusz Szczudlak <mateusz.szczudlak@gmail.com>
2020-01-15 19:28:28 +0100

    update1

--- features/step_definitions/definitions.js
+++ features/step_definitions/definitions.js
@@ -2,45 +2,43 @@ const { Given, When, Then } = require('cucumber')
 const assert = require('assert');
 const { driver } = require('../support/get_driver');
 const { $ } = require('webdriverio');
+const CukeTest = require('cuketest');
 
 ////step definitions/////
-
-sleep = (ms) => new Promise(res => setTimeout(res, ms));
-
 
 Given("I open the FB app in my android device", async function () {
     let fb = driver.element("~Facebook");
     fb.click();
-    await sleep(4000);
+    await CukeTest.delay(4000);
 });
 
 When("I enter username {string}", async function (keyword) {
     let username = driver.element("~Username");
     username.setValue(keyword);
-    await sleep(2500)
+    await CukeTest.delay(2500);
 });
 
 When("I enter password {string}", async function (keyword) {
     let pass = driver.element("~Password");
     pass.setValue(keyword);
-    await sleep(2500);
+    await CukeTest.delay(2500);
 });
 
 When("I click on login button", async function () {
-    await sleep(3000);
+    await CukeTest.delay(3000);
     let login = driver.element("~Login");
     login.click();
-    await sleep(2000);
+    await CukeTest.delay(2000);
 });
 
 Then("I see the error message {string}", async function (keyword) {
-    await sleep(3500);
+    await CukeTest.delay(3500);
     let el10 = driver.element("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.widget.TextView");
     return assert.equal(await el10.getText(), keyword);
 });
 
 Then("Login button is disabled", async function () {
-    await sleep(1500);
+    await CukeTest.delay(1500);
     let login = driver.element("~Login");
     return assert.equal(await login.isEnabled(),0);
 });
@@ -51,9 +49,9 @@ Then("I click on the Try Again button to get back to the login page", async func
 });
 
 Then("I close the FB app", async function () {
-    await sleep(1000);
+    await CukeTest.delay(1000);
     await driver.closeApp();
-    await sleep(1000);
+    await CukeTest.delay(1000);
 });
 
 Given("email and password fields are empty", async function () {
